54117ecdfeea1136b0b0026e2aa1374459a9baba
Hillary Okello <hillariouskelly@outlook.com>
2020-11-25 11:53:21 +0300

    wrote psedo code

---
+++ main.rb
@@ -0,0 +1,7 @@
+#define a method which takes in an array as its parameter
+#looping through the array
+#set the value of swap to false
+#if the lenght of array is 1 or less than 1 we shouldn't swap
+#comparison of first and second elements 
+#If the element on the left is greater than the one on the right swap the two
+
